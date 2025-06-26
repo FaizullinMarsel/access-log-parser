aa1ea669c9f20532c8c057db266f06a70a9946ba
Marsel Faizullin <f1marsel@gmail.com>
2025-06-26 13:44:47 +0300

    Добавлена новая ветка 2-number-experiments. В классе main обновлена логика работы программы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Введите текст и нажмите <Enter>: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Результаты");
+        System.out.println("Сумма: " + firstNumber + secondNumber);
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + firstNumber * secondNumber);
+        System.out.println("Частное: " + (double)firstNumber/secondNumber);
     }
 }
